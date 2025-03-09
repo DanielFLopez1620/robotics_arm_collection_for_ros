e068cd8e178bede3b58a495dff3acdcaf6c92d06
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2025-03-09 11:57:59 -0500

    Eezybot MK2 Description: Added link 135 and horarm, pending fixing orientation

--- eezybot_mk2_description/launch/display.launch.py
+++ eezybot_mk2_description/launch/display.launch.py
@@ -5,6 +5,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
@@ -31,9 +32,10 @@ def generate_launch_description():
         name="robot_state_publisher",
         output='screen',
         parameters=[{
-            'robot_description': Command([
-                'xacro ', xacro_file, 
-            ])
+            'robot_description': ParameterValue(
+                Command(['xacro ', xacro_file, 
+                ]), value_type=str
+            )
         }]
     )
 
