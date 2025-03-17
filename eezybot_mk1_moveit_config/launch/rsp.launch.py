bf9bf4816487311b5506b446c57cd81161d0fcc7
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2025-03-17 15:12:03 -0500

    MK1 Description: Expanded URDF try | MK1 Moveit: First try with Moveit but failed

---
+++ eezybot_mk1_moveit_config/launch/rsp.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_rsp_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("eezybot_mk1", package_name="eezybot_mk1_moveit_config").to_moveit_configs()
+    return generate_rsp_launch(moveit_config)
